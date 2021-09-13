7e5eed66ee89aa61efa1ad0b3392fff34810a370
muba23 <muba2307@gmail.com>
2021-09-13 13:24:06 +0600

    total problem solved

--- js/app.js
+++ js/app.js
@@ -15,19 +15,20 @@ const showProducts = products => {
             const div = document.createElement("div");
             div.classList.add("product");
             div.innerHTML = `
-          <div class="single-product bg-warning bg-opacity-10">
+          <div class="card single-product bg-success bg-opacity-10">
             <div>
               <img class="product-image" src=${image}></img>
             </div>
             <h3>${product.title}</h3>
             <p>Category: ${product.category}</p>
             <h2>Price: $ ${product.price}</h2>
-            <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">Add to cart</button>
-            <button id="details-btn" class="btn btn-danger">Details</button></div>
+            <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn " style="background-color: #0080ff; color: white">Add to cart</button>
+            <button id="details-btn" class="btn" style="background-color:#ffbf00">Details</button></div>
             `;
           document.getElementById("all-products").appendChild(div);
   }
 };
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -35,11 +36,12 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  updateTotal(); //updateTotal function added
 };
 
 const getInputValue = id => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -76,5 +78,6 @@ const updateTaxAndCharge = () => {
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal = getInputValue("price") + getInputValue("delivery-charge") + getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  const grandTotalFloat = parseFloat(grandTotal).toFixed(2);
+  document.getElementById("total").innerText = grandTotalFloat;
 };
